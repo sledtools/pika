b6942d95486efca012d0b0282ce6cb8332833e42
Justin Moon <mail@justinmoon.com>
2026-03-09 00:45:01 -0500

    Cut staged Linux Rust execute lane to x86 microvm

--- nix/ci/linux-rust.nix
+++ nix/ci/linux-rust.nix
@@ -207,6 +207,7 @@ rec {
 
   workspaceBuild = craneLib.mkCargoDerivation (commonArgs // {
     pname = "${commonArgs.pname}-build";
+    src = workspaceDeps.src;
     cargoArtifacts = workspaceDeps;
     doCheck = false;
     buildPhaseCargoCommand = laneCompileCommand;
